7d1e1a2368f933c5a585c8328ccc95279c970402
shweta1611 <67777664+shweta1611@users.noreply.github.com>
2022-05-30 16:20:31 +0530

    Delete src directory

--- src/main/java/onlinefood/service/IOrderService.java
+++
@@ -1,9 +0,0 @@
-package onlinefood.service;
-
-import onlinefood.model.OrderDetails;
-
-public interface IOrderService {
-	public OrderDetails addOrder(OrderDetails order);
-	public OrderDetails getOrder(long orderId);
-	public OrderDetails deleteOrder(long orderId);
-}
